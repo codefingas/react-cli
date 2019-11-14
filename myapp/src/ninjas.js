a5f4ee16bd300ba45ab4cbeced0dc6c9432bf070
Ezekiel Saturday <ezekielsaturday@Grooves-MacBook-Pro.local>
2019-11-14 12:10:38 +0100

    explored conditional rendering using if and tenary operator

--- myapp/src/ninjas.js
+++ myapp/src/ninjas.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
-const Ninjas = (props) => {//in functional components have to be explicitly Referenced 
+const Ninjas = (prop) => {//in functional components have to be explicitly Referenced 
     //we can destructure the parameter directly by passing {ninjas} instead of props and destructuring afterwards
-        const {ninjas} = this.props,
-            ninjaList = ninjas.map(ninja => {
-                return (
-                     <div className="ninja" key={ninja.id}>
-                        <div>Name : {ninja.name}</div>
-                        <div>Age : {ninja.age}</div>
-                        <div>Belt : {ninja.belt}</div>
-                    </div>
-                )
-            });
+        const {ninjas} = prop,
+                ninjaList = ninjas.map(ninja => {
+                    //using tenary operator to 
+                   return ninja.age > 20 ? (
+                        <div className="ninja" key={ninja.id}>
+                            <div>Name : {ninja.name}</div>
+                                <div>Age : {ninja.age}</div>
+                                <div>Belt : {ninja.belt}</div>
+                        </div>
+                   ) : null;
+                })
+
+            // ninjaList = ninjas.map(ninja => {
+            //     if (ninja.age > 20) {
+            //         return (
+            //             <div className="ninja" key={ninja.id}>
+                                {/* <div>Name : {ninja.name}</div>
+                                    <div>Age : {ninja.age}</div>
+                                    <div>Belt : {ninja.belt}</div>
+                            </div> */}
+            //         )
+            //     } else {
+            //         return null
+            //     }
+            // });
+
+
 
         return (
            <div className="ninja-list">
