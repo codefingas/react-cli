5a3bd2b940453791a2db590b6939f36939076a91
Ezekiel Saturday <ezekielsaturday@Grooves-MacBook-Pro.local>
2019-11-14 03:46:25 +0100

    explored nesting components

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Ninjas from './ninjas';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        My first react app
+      </h1>
+      <p>Welcome</p>
+      <Ninjas />
+    </div>
+  );
+}
+
+export default App;
