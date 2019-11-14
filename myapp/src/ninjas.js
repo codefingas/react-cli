f08b1514bb3bd3f1edf4c4a15dd325aab19dd97e
Ezekiel Saturday <ezekielsaturday@Grooves-MacBook-Pro.local>
2019-11-14 10:18:06 +0100

    changed the Ninjas components to a ui-component by creating it with a function instead of a class

--- myapp/src/ninjas.js
+++ myapp/src/ninjas.js
@@ -1,17 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Ninjas extends Component {
-    render(){
-        //console.log(this.props)
-        const {name, age, belt } = this.props;
+const Ninjas = (props) => {//in functional components have to be explicitly Referenced 
+    //we can destructure the parameter directly by passing {ninjas} instead of props and destructuring afterwards
+        const {ninjas} = this.props,
+            ninjaList = ninjas.map(ninja => {
+                return (
+                     <div className="ninja" key={ninja.id}>
+                        <div>Name : {ninja.name}</div>
+                        <div>Age : {ninja.age}</div>
+                        <div>Belt : {ninja.belt}</div>
+                    </div>
+                )
+            });
+
         return (
-            <div className="ninja">
-                <div>Name : {name}</div>
-                <div>Age : {age}</div>
-                <div>Belt : {belt}</div>
-            </div>
+           <div className="ninja-list">
+               {ninjaList}
+           </div>
         )
-    }
 }
 
 export default Ninjas;
