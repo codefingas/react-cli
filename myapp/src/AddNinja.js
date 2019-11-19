fb9a33ea5743422d94591649b87be3c6cf83a5b4
Ezekiel Saturday <ezekielsaturday@Grooves-MacBook-Pro.local>
2019-11-19 15:35:34 +0100

    created form for addingninjas

---
+++ myapp/src/AddNinja.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class AddNinja extends Component {
+        render() {
+            return (
+                <form>
+                    <label htmlFor="name">
+                        Name:
+                    </label>
+                    <input type="text" name="" id="name" onChange={}/>
+
+                    <label htmlFor="name">
+                        Name:
+                    </label>
+                    <input type="text" name="" id="name" onChange={}/>
+
+                    <label htmlFor="name">
+                        Name:
+                    </label>
+                    <input type="text" name="" id="name" onChange={}/>
+
+                    <label htmlFor="name">
+                        Name:
+                    </label>
+                    <input type="text" name="" id="name" onChange={}/>
+                </form>
+            )
+        }
+}
+
+export default AddNinja;
